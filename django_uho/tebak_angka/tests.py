449fa61145fe05f2aff7d7e629e43d5345b77bc1
mriansyah93 <mriansyah93@gmail.com>
2024-12-26 06:21:12 +0800

    solve real gemini request

--- django_uho/tebak_angka/tests.py
+++ django_uho/tebak_angka/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+import os
+import requests
 
-# Create your tests here.
+class RunGemini(TestCase):
+    def test_api_real_gemini(self):
+        pass
+
+    def test_cli_real_gemini(self):
+        api_key = os.getenv("GEMINI_API_KEY")
+        data = {
+            "prompt": "say hi!",
+            "temperature": 0.7,  
+            "maxTokens":  100  
+        }
+
+        gemini_url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"
+        headers = {"Content-Type": "application/json"}
+        gemini_payload = {"contents": [{
+            "parts": [{"text": data.get("prompt")}]
+        }]}
+
+        # Send the request to the Gemini API
+        response = requests.post(f"{gemini_url}?key={api_key}", headers=headers, json=gemini_payload)
+
+        gemini_response = response.json()
+        # print(gemini_response)
+        response_content = gemini_response['candidates'][0]['content']['parts'][0]['text']
+        finish_reason = gemini_response['candidates'][0]['finishReason']
+        avg_logprobs = gemini_response['candidates'][0].get('avgLogprobs')
+        prompt_token_count = gemini_response['usageMetadata']['promptTokenCount']
+        candidates_token_count = gemini_response['usageMetadata']['candidatesTokenCount']
+        total_token_count = gemini_response['usageMetadata']['totalTokenCount']
+        model_version = gemini_response['modelVersion']
+
+        # Assertions
+        self.assertIsNotNone(response_content) 
+        self.assertIsNotNone(finish_reason) 
+        # Check if avgLogprobs exists (it might be optional)
+        if avg_logprobs:
+            self.assertIsInstance(avg_logprobs, float) 
+        self.assertIsInstance(prompt_token_count, int)
+        self.assertIsInstance(candidates_token_count, int)
+        self.assertIsInstance(total_token_count, int)
+        self.assertIsNotNone(model_version) 
--- django_uho/tebak_angka/urls.py
+++ django_uho/tebak_angka/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('gemini_log', views.get_logs, name='log gemini'),
+    path('real-ai-submit', views.real_ai_submit, name='real AI submit'),
+    path('token-csrf', views.get_csrf_token, name='token csrf'),
+    
+    path('mock-ai-submit', views.mock_ai_submit, name='mock AI submit'),
+    path('ai-log', views.get_logs, name='log of ai usage'),
+    path('usage-summary', views.get_aggregates, name='aggregate log'),
+    path('usage-limit', views.check_usage_limit, name='AI limit'),
+    
     path('cnn_with_log', views.cnn_prediction_log, name='cnn_prediction'),
     path('prediction_log_list', views.prediction_log_list, name='prediction_log_list'),
 ]

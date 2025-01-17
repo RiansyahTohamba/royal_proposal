8836803ecffb4210ada0e382b719f77f56ed00fa
RiansyahTohamba <mriansyah93@gmail.com>
2025-01-17 08:58:15 +0700

    finish CRUD of candidates

--- django_uho/ai_hr/serializers.py
+++ django_uho/ai_hr/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-# from .models import Candidate, JobCriteria, InterviewStage
-from .models import Candidate
+from .models import Candidate, JobCriteria, InterviewStage
 
 class CandidateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidate
+        fields = '__all__'
+
+class JobCriteriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobCriteria
+        fields = '__all__'
+
+class InterviewStageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InterviewStage
         fields = '__all__'
--- django_uho/ai_hr/views.py
+++ django_uho/ai_hr/views.py
@@ -1,20 +1,36 @@
-
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from .models import Candidate
-from .serializers import CandidateSerializer
+from .models import Candidate, JobCriteria, InterviewStage
+from .serializers import CandidateSerializer, JobCriteriaSerializer, InterviewStageSerializer
 
 class CandidateViewSet(viewsets.ModelViewSet):
     queryset = Candidate.objects.all()
     serializer_class = CandidateSerializer
-    @action(detail=True, methods=['get'])
-    def get_candidate(self):
-        return Response(candidates=[],status=status.HTTP_200_OK)
 
-    # @action(detail=True, methods=['post'])
-    # def upload_cv(self, request, pk=None):
-    #     candidate = self.get_object()
-    #     candidate.cv = request.data['cv']
-    #     candidate.save()
-    #     return Response(status=status.HTTP_200_OK)
+    @action(detail=True, methods=['post'])
+    def upload_cv(self, request, pk=None):
+        candidate = self.get_object()
+        candidate.cv = request.data['cv']
+        candidate.save()
+        return Response(status=status.HTTP_200_OK)
+
+class JobCriteriaViewSet(viewsets.ModelViewSet):
+    queryset = JobCriteria.objects.all()
+    serializer_class = JobCriteriaSerializer
+
+    @action(detail=True, methods=['post'])
+    def predict_job_suitability(self, request, pk=None):
+        # Placeholder for job suitability prediction logic
+        job_criteria = self.get_object()
+        # Do some predictions based on the provided stages
+        return Response({'result': 'Prediction result'}, status=status.HTTP_200_OK)
+
+class InterviewStageViewSet(viewsets.ModelViewSet):
+    queryset = InterviewStage.objects.all()
+    serializer_class = InterviewStageSerializer
+
+    @action(detail=False, methods=['post'])
+    def conduct_virtual_interview(self, request):
+        # Placeholder for virtual interview stage logic
+        return Response({'result': 'Interview conducted'}, status=status.HTTP_200_OK)

5d3ec12fda9281fc067e698c3a4d308c29a87088
mriansyah93 <mriansyah93@gmail.com>
2025-04-26 20:25:46 +0800

    remove key

--- django_uho/docs/curl.sh
+++
@@ -1,8 +0,0 @@
-curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyDogu07v-gFGs5F83bTm-nf8SOnQA6E2BQ" \
--H 'Content-Type: application/json' \
--X POST \
--d '{
-  "contents": [{
-    "parts":[{"text": "what is Artificial intelligence?"}]
-    }]
-   }'
